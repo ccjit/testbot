b95ad6d04f318dda48c87c47fa535b9e4a4003cf
ccjt <circjit@gmail.com>
2025-05-04 19:55:13 -0300

    Update tampermonkey.js

--- tampermonkey.js
+++ tampermonkey.js
@@ -1,26 +1,27 @@
 // ==UserScript==
 // @name         TestBot - Tampermonkey Version
-// @namespace    https://github.com/ccjit/testbot/blob/main/main.js
-// @version      v1.0-alpha7
+// @namespace    https://ccjit.github.io/my-site
+// @version      v1.0-alpha8
 // @description  take over the world, not really, much
-// @author       ccjt & TestGirl
-// @match        https://multiplayerpiano.net/*
+// @author       ccjt & Test Account
+// @match        https://piano.mpp.community/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=multiplayerpiano.net
 // @grant        none
 // ==/UserScript==
 (function() {
     'use strict';
+    let version = "1.0-alpha8"
     let mem = "lobby"
-    if (localStorage("whitelist") == undefined) {
+    if (localStorage.whitelist == undefined) {
         let whitelist = [
             'bfcd3dabed0ab33cc18cebf5', // ccjt
-            '1f2fd4b5d634d96f0a193ae8', // ccjt too
-            '8703f0cc55518aade8cbcffe', // testgirl
+            '896c8c5975b93481d11ccb33', // ccjt too
+            '1f2fd4b5d634d96f0a193ae8', // Bot's ID
+            '8703f0cc55518aade8cbcffe' // Test Account
         ]
         localStorage.setItem("whitelist", whitelist);
-    } else {
-        let whitelist = localStorage("whitelist")
     }
+    let whitelist = localStorage.whitelist
     // random response area
     let inforesponses = [
         "I didn't even tell you you can't do that! Wait, that's my fault. Just know that this also doesn't work.",
@@ -41,7 +42,7 @@
     let ownerresponses = [
         "Seriously. You're an owner and you ***still*** do this? What kinda level of brain damage do you have in that headphone-dented head of yours?",
         "Wow. You have this much power yet still commit the same rookie mistake.",
-        "You know what? If the command name is \"owner\", I might aswell make it do something useful. Bot owner: ccjt & TestGirl"
+        "You know what? If the command name is \"owner\", I might aswell make it do something useful. Bot owner: ccjt & Test Account"
     ]
     let personalresponses = [
         "You know what, I predicted this. If changing the wrong example for owners to be \"!personal\" instead of \"!fun\", I basically called it and knew this would happen. Checkmate.",
@@ -81,7 +82,7 @@
         "Oops! That category doesn't exist. Please try one of these categories: ",
         "I couldn't find this category. Please try again. Categories: ",
         "Did you intend for this to happen or not? Anyway, here are the categories: ",
-        "Uncaught ReferenceError: " + a[1] + " is not a valid category. Categories: ",
+        "Uncaught ReferenceError: That is not a valid category. Categories: ",
         "If you see this response message, you either were spamming invalid categories or you're just plain lucky. Valid categories: "
     ]
     let e = false
@@ -89,22 +90,22 @@
     let space = false
     let oldhelp = false
     // startup message
-    MPP.chat.send("TestBot is on. Use〈!help〉to use commands.")
+    MPP.chat.send("TestBot is on. Use 〈 !help 〉 to see commands.")
     // main script
     MPP.client.on("a", function (m) {
         let a = m.a.split(" ");
         let c = a[0].toLowerCase();
-        let crown = MPP.client.channel.crown.userId
         let name = m.p.name
         let color = m.p.color
         let tid = m.p.id
         let id = m.p._id
-        let whitelisted = function(id){if(whitelist.indexOf(id)<-1){return true}else{return false}}
+        let crowned = function() { if ('crown' in MPP.client.channel) { if (MPP.client.channel.crown.userId == MPP.client.getOwnParticipant()._id) { return true; } else { return false; } } else { return false; } }
+        let whitelisted = function(id){if(whitelist.includes(id)){return true}else{return false}}
         let ppl = function(id, info){return eval(`MPP.client.ppl[${id}].${info}`)}
         let substring = function(a){return m.a.substring(a).trim();}
         let send = function(a){MPP.chat.send(a);}
         let kick = function(id, ms){MPP.client.sendArray([{"m":"kickban","_id":id,"ms":ms}]);}
-        let whitelist = function(id){whitelist.push(id);localStorage.setItem("whitelist", whitelist);}
+        let addToWhitelist = function(id){whitelist.push(id);localStorage.setItem("whitelist", whitelist);}
         // commands
         if (c == "!fun") {
             send(funresponses[Math.floor(Math.random() * funresponses.length)])
@@ -122,9 +123,9 @@
             send(facts[Math.floor(Math.random() * facts.length)])
         }
         if (c == "!dice") {
-            if (a.length == 0) {
+            if (a.length == 1) {
                 send(`${Math.floor(Math.random() * 20)}`)
-            } else if (a.length == 1) {
+            } else if (a.length == 2) {
                 send(`${Math.floor(Math.random() * parseInt(a[1]))}`)
             } else {
                 send(`${Math.floor(Math.random() * parseInt(a[2]) + parseInt(a[1]))}`)
@@ -134,9 +135,9 @@
             send("your ID is " + id)
         }
         if (c == "!about") {
-            send("TestBot - Made by ccjt & TestGirl | Programming: ccjt - Ideas: TestGirl | This bot is open-source on GitHub and free for you to use. | Made in 2025")
+            send("TestBot - Made by ccjt & Test Account | Programming: ccjt - Ideas: Test Account | This bot is open-source on GitHub and free for you to use. | Made in 2025 | Running version " + version)
             send("GitHub page for TestBot: https://github.com/ccjit/testbot/")
-            if (a.length == 1) {
+            if (a.length == 2) {//
                 send(`${a[1]}'s info - Name: ${ppl(a[1], "name")}, Color: ${ppl(a[1], "color")}, Mouse Position: X ${ppl(a[1], "mouseX")} Y ${ppl(a[1], "mouseY")}, AFK: ${ppl(a[1], "afk")}`)
             }
         }
@@ -144,14 +145,29 @@
             send(ball[Math.floor(Math.random() * ball.length)])
         }
         if (c == "!say") {
-            if (whitelist.indexOf(id) > -1) {
+            if (whitelisted(id)) {
                 send(substring(4))
             } else {
                 send(name + ": " + substring(4))
             }
         }
         if (c == "!currevents") {
-            send("Event statuses: E: " + e ? "on" : "off" + " - The: " + the ? "on" : "off" + " - Space: " + space ? "on" : "off")
+            send("Events:")
+            if (e) {
+                send("E: on")
+            } else {
+                send("E: off")
+            }
+            if (the) {
+                send("The: on")
+            } else {
+                send("The: off")
+            }
+            if (space) {
+                send("Space: on")
+            } else {
+                send("Space: off")
+            }
         }
         if (c == "!stopevents") {
             if (e) {
@@ -168,7 +184,8 @@
             }
         }
       if (c == "!help") {
-              if (a.length == 0) {
+          if (!oldhelp) {
+              if (a.length == 1) {
                   if (whitelisted(id)) {
                       send("Categories: info, fun, personal, owner - use !help [category] to find the commands in it")
                       send("Example: !help personal")
@@ -179,13 +196,12 @@
                       send("Wrong example: !fun")
                   }
               } else {
-                  if (!oldhelp) {
                       if (a[1] == "info") {
                           send("!userid - Tells your user id | !about - Bot info OR get info from user - Usage: !about (returns bot info), !about [id] (returns user info)")
                       } else if (a[1] == "fun") {
                           send("!dice - Returns a random number between 1 and 20 | !8ball - Shake a Magic 8 Ball™ and get a response. | !say -  Makes the bot say something. | !funfact - Tells you a randomly picked fact.")
                       } else if (whitelisted(id) && a[1] == "owner") {
-                          if (crown == MPP.client.participantId) {
+                          if (crowned()) {
                               send("Owner commands: !kick - Kicks someone - Usage: !kick [id] [seconds] [one word reason] | !crown - Gives crown to ID - Usage: !crown [id] | !say (owner's version) - Makes the bot say something, but with no added text. !oldhelp - Toggles old help command on/off. | !flashbang - Flashbangs everyone without force dark background on. ||[1]||")
                               send("!setting - Changes a room setting. | !whitelist - Whitelists ID, potentially dangerous. ||[2]||")
                           } else {
@@ -194,26 +210,26 @@
                       } else if (whitelisted(id) && a[1] == "personal") {
                           send("Personal commands: !name - Sets the bot name. | !events - Check list of possible events. | !stopevents - Stop all currently running events. | !currevents - See what events are happening right now.")
                       }
-                      if (whitelisted(id)) {
+                      if (whitelisted(id) && a[1] != "info" && a[1] != "fun" && a[1] != "personal" && a[1] != "owner") {
                           send(categoryresponses[Math.floor(Math.random()*categoryresponses.length)] + "info, fun, personal, owner")
-                      } else {
+                      } else if (!whitelisted(id) && a[1] != "info" && a[1] != "fun"){
                           send(categoryresponses[Math.floor(Math.random()*categoryresponses.length)] + "info, fun")
                       }
                   }
-              }
           } else {
               send("Commands: !userid - Tells your user id | !dice - Returns a random number between 1 and 20 | !8ball - Shake a Magic 8 Ball™ and get a response. | !say - Makes the bot say what you command it to do | !about - Bot info OR get info from user - Usage: !about (returns bot info), !about [id] (returns user info) ||[1]||")
               send("!funfact - Tells you a randomly picked fact. ||[2]||")
-              if (id == MPP.client.participantId || whitelist.includes(id)) {
-                  if (crown == MPP.client.participantId || whitelist.includes(id)) {
+              if (whitelisted(id)) {
+                  if (whitelisted(id)) {
                       send("Owner commands: !kick - Kicks someone - Usage: !kick [id] [seconds] [one word reason] | !crown - Gives crown to ID - Usage: !crown [id] | !name - Sets the bot name. | !say (owner's version) - Makes the bot say something, but with no added text. !oldhelp - Toggles old help command on/off. ||[1]||")
                       send("!setting [room setting] - Changes desired room setting. ||[2]||")
                       send("Personal commands: !events - Check list of possible events")
                   }
               }
           }
+              }
         // personal command zone
-        if (id == MPP.client.participantId || whitelist.includes(id)) {
+        if (whitelisted(id)) {
             if (c == "!events") {
                 send("Events: !e - kicks people who say the letter E for 0 minutes | !the - bans the word \"the\" from being sent in chat | !spacebar - kick user who sent a messsge with a space anywhere on it")
             }
@@ -233,7 +249,7 @@
                         "name": substring(5)
                     }
                 }])
-                send()
+                send("Name set to " + substring(5))
             }
             if (c == "!flashbang") {
                 MPP.client.sendArray([{
@@ -256,11 +272,11 @@
         // whitelisted users street
         if (whitelisted(id)) {
             if (c == "!whitelist") {
-                if (a.length == 0) {
+                if (a.length == 1) {
                     send("Please specify an ID to whitelist.")
-                } else if (a.length == 1) {
-                    send("Are you sure you want to do this? Send `" + a[0] + " " + a[1] + " y` to confirm.")
                 } else if (a.length == 2) {
+                    send("Are you sure you want to do this? Send `" + a[0] + " " + a[1] + " y` to confirm.")
+                } else if (a.length == 3) {
                     if (a[2] == "y") {
                         whitelist(a[1])
                         send("Successfully whitelisted. Amount of Whitelisted IDs: " + whitelist.length)
@@ -268,7 +284,7 @@
                 }
             }
             if (c == "!crown") {
-                if (crown == MPP.client.participantId || (crown == MPP.client.participantId && whitelist.includes(id))) {
+                if (crowned() || (crowned() && whitelisted(id))) {
                     MPP.client.sendArray([{
                         "m": "chown",
                         "id": JSON.stringify(substring(5))
@@ -278,7 +294,7 @@
                 }
             }
             if (c == "!kick") {
-                if (crown == MPP.client.participantId || (crown == MPP.client.participantId && whitelist.includes(id))) {
+                if ((crowned() && whitelisted(id))) {
                     let mem = MPP.client.desiredChannelId
                     kick(a[1], parseInt(a[2]))
                     MPP.client.setChannel('test/awkward')
@@ -290,15 +306,15 @@
             }
             if (c == "!oldhelp") {
                 if (oldhelp) {
-                    send("Help command is now in the new format.")
                     let oldhelp = false
+                    send("Help command is now in the new format.")
                 } else {
-                    send("Help command is now in the old format.")
                     let oldhelp = true
+                    send("Help command is now in the old format.")
                 }
             }
         }
-        if (crown == MPP.client.participantId) {
+        if (crowned()) {
             if (c == "!setting") {
                 if (a[1] == "color") {
                     MPP.client.sendArray([{
@@ -343,17 +359,17 @@
         // event checks
         if (id != MPP.client.participantId) {
             if (e) {
-                if (m.a.indexOf('e') > -1) {
+                if (m.a.includes('e')) {
                     kick(id, 0)
                 }
             }
             if (the) {
-                if (m.a.indexOf('the') > -1) {
+                if (m.a.includes('the')) {
                     kick(id, 0)
                 }
             }
             if (space) {
-                if (m.a.indexOf(' ') > -1) {
+                if (m.a.includes(' ')) {
                     kick(id, 0)
                 }
             }
